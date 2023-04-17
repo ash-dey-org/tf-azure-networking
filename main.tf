ee0c83b18c5b69886f89159010a74c59be1c120a
Ash Dey <ashabc [at] hotmail.com>
2023-04-17 10:20:00 +1000

    fixed formatting

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "vnet" {
-    source = "./modules/vnet"
+  source = "./modules/vnet"
 
-    for_each = toset(var.environments)
+  for_each = toset(var.environments)
 
-    env = each.key
-    location = var.location-prod
-    company-name = var.company-name
-    address_space = var.address_space
-    rg-name = var.rg-name
-    vnet-name = var.vnet-name
+  env           = each.key
+  location      = var.location-prod
+  company-name  = var.company-name
+  address_space = var.address_space
+  rg-name       = var.rg-name
+  vnet-name     = var.vnet-name
 }
